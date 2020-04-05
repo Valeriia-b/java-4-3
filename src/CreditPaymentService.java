23ae3d23af000de4431255c89c9a43ab95ab18c4
Valeriia <lera-bezborodova@mail.ru>
2020-04-05 21:06:17 +0300

    Fixed comments

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
-    public double calculate (int amount, int period, double percent) {
-        int periodMonth = period *12; // период в месяцах
+    public double calculate(int amount, int period, double percent) {
+        int periodMonth = period * 12; // период в месяцах
         double rate = percent / 12 / 100; // ставка в месяц
-     double payment = amount * (rate * Math.pow((1+rate),periodMonth)/(Math.pow((1+rate),periodMonth) -1));
-     return payment;
+        double payment = amount * (rate * Math.pow((1 + rate), periodMonth) / (Math.pow((1 + rate), periodMonth) - 1));
+        return payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int payment1year = (int) (service.calculate(1_000_000,1,9.99)); System.out.println(payment1year);
-        int payment2years = (int) (service.calculate(1_000_000,2,9.99)); System.out.println(payment2years);
-        int payment3years = (int) (service.calculate(1_000_000,3,9.99)); System.out.println(payment3years);
+        int payment1year = (int) (service.calculate(1_000_000, 1, 9.99));
+        System.out.println(payment1year);
+        int payment2years = (int) (service.calculate(1_000_000, 2, 9.99));
+        System.out.println(payment2years);
+        int payment3years = (int) (service.calculate(1_000_000, 3, 9.99));
+        System.out.println(payment3years);
     }
 }
